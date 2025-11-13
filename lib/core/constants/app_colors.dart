022c29fde04b484a815c385b841790dcf55c92a6
kamurandogan <kamurandgn@gmail.com>
2025-11-13 14:25:51 +0300

    app_colors added

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primaryColor = Color(0xFFf7f6f0);
+  static const Color secondaryColor = Color(0xFF000000);
+  static const Color backgroundColor = Color(0xFFf7f6f0);
+}
